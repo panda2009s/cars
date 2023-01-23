a966e14c47098ae6c5f47f24b4cb940ae32d49fc
mihailponomarev <maikalpanda2009@yandex.ru>
2023-01-23 11:53:51 +0300

    task1.1

---
+++ src/main/java/Car/Car2.java
@@ -0,0 +1,28 @@
+package Car;
+
+public class Car2 {
+
+    String color;
+    String type;
+    int year;
+
+    public void getColor() {
+        System.out.println(this.color);
+    }
+    public void getType() {
+        System.out.println(this.type);
+    }
+    public void getYear() {
+        System.out.println(this.year);
+    }
+    public void getCarStart() {
+        System.out.println("Автомобиль заведен");
+    }
+    public void getCarStop() {
+        System.out.println("Автомобиль заглушен");
+    }
+    public Car2 () {
+    }
+
+
+}
---
+++ src/main/java/Car/Main2.java
@@ -0,0 +1,17 @@
+package Car;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+    Car2 car2 = new Car2();
+    car2.color = "Blue";
+    car2.type = "Минивэн";
+    car2.year = 2020;
+    car2.getCarStart();
+    car2.getColor();
+    car2.getType();
+    car2.getYear();
+    car2.getCarStop();
+    }
+
+}
